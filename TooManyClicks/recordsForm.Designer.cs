20a4d35cb191810fe46dfcbbbd91948b31308fe2
Enrique Bazua <enriquegbazua@gmail.com>
2022-01-27 02:52:59 -0700

    Records Form

---
+++ TooManyClicks/recordsForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace TooManyClicks
+{
+    partial class recordsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(recordsForm));
+            this.titleRecordLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleRecordLbl
+            // 
+            this.titleRecordLbl.AutoSize = true;
+            this.titleRecordLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleRecordLbl.ForeColor = System.Drawing.Color.White;
+            this.titleRecordLbl.Location = new System.Drawing.Point(122, 25);
+            this.titleRecordLbl.Name = "titleRecordLbl";
+            this.titleRecordLbl.Size = new System.Drawing.Size(212, 42);
+            this.titleRecordLbl.TabIndex = 0;
+            this.titleRecordLbl.Text = "RECORDS";
+            // 
+            // recordsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(465, 392);
+            this.Controls.Add(this.titleRecordLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "recordsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Records";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleRecordLbl;
+    }
+}
