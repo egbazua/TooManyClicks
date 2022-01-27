99360291c5552c66c2f35bd192c09f18e64abc1a
Enrique Bazua <enriquegbazua@gmail.com>
2022-01-27 16:41:41 -0700

    recordsForm: re-design

--- TooManyClicks/recordsForm.Designer.cs
+++ TooManyClicks/recordsForm.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.titleRecordLbl.AutoSize = true;
             this.titleRecordLbl.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleRecordLbl.ForeColor = System.Drawing.Color.White;
+            this.titleRecordLbl.ForeColor = System.Drawing.Color.Red;
             this.titleRecordLbl.Location = new System.Drawing.Point(163, 26);
             this.titleRecordLbl.Name = "titleRecordLbl";
             this.titleRecordLbl.Size = new System.Drawing.Size(199, 51);
@@ -47,6 +47,10 @@
             // 
             // recordsDgv
             // 
+            this.recordsDgv.AllowUserToAddRows = false;
+            this.recordsDgv.AllowUserToDeleteRows = false;
+            this.recordsDgv.AllowUserToResizeRows = false;
+            this.recordsDgv.BackgroundColor = System.Drawing.Color.White;
             this.recordsDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.recordsDgv.Location = new System.Drawing.Point(38, 104);
             this.recordsDgv.Name = "recordsDgv";
