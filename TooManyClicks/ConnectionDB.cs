a31d5289377cd3be592307bdc4e709509c4afdda
Enrique Bazua <enriquegbazua@gmail.com>
2022-01-27 03:31:33 -0700

    Connection to SQL Database

--- TooManyClicks/ConnectionDB.cs
+++ TooManyClicks/ConnectionDB.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace TooManyClicks
 {
     internal class ConnectionDB
     {
+        string chain = "Data Source=EGBAZUA-PC;Initial Catalog=TooManyClicks; Integrated Security=True";
+        public SqlConnection myConnection = new SqlConnection();
+
+        public ConnectionDB()
+        {
+            myConnection.ConnectionString = chain;
+        }
+
+        public void open()
+        {
+            try
+            {
+                myConnection.Open();
+                Console.WriteLine("Connection open");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Connection error" + ex.Message);
+            }
+        }
+
+        public void close()
+        {
+            myConnection.Close();
+        }
     }
 }
